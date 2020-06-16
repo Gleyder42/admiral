4b2decb666925d8cd20b018b6551fa36705a0442
Gleyder <Gleyder@t-online.de>
2020-06-16 12:52:05 +0200

    Added ValueBag tests

--- src/test/java/de/gleyder/admiral/AdmiralTest.java
+++ src/test/java/de/gleyder/admiral/AdmiralTest.java
@@ -3,16 +3,14 @@ package de.gleyder.admiral;
 import de.gleyder.admiral.interpreter.IntegerInterpreter;
 import de.gleyder.admiral.node.CommandNode;
 import de.gleyder.admiral.node.NodeKey;
-import de.gleyder.admiral.node.TypeNoteKey;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -24,26 +22,53 @@ public class AdmiralTest {
   private final CommandSolver solver = new CommandSolver();
 
   private final CommandNode testNode = new CommandNode(NodeKey.ofStatic("test"));
-  private final CommandNode echoNode = new CommandNode(NodeKey.ofStatic("echo"));
+  private final CommandNode echoNode = new CommandNode(NodeKey.ofStatic("echo"))
+          .setExecutor(context -> log.info("This echo node has an executor but not a value!"));
+  private final CommandNode createNode = new CommandNode(NodeKey.ofStatic("create"));
 
-  private final CommandNode stringLogNode = new CommandNode(NodeKey.ofDynamic(String.class, "string"));
+  private final CommandNode stringLogNode = new CommandNode(NodeKey.ofDynamic(String.class, "string"))
+          .setExecutor(context -> log.info("Echo {}", context.getBag().get("string").orElseThrow()));
 
   private final CommandNode integerLogNode = new CommandNode(NodeKey.ofDynamic(Integer.class, "int"))
+          .setExecutor(context -> log.info("Int Echo: {}", context.getBag().get("int").orElseThrow()))
           .setInterpreter(new IntegerInterpreter());
+
+  private final CommandNode optionalNode = new CommandNode(NodeKey.ofStatic("optional"))
+          .setExecutor(context -> {
+            Optional<Integer> anInt = context.getBag().get("int");
+            Optional<String> string = context.getBag().get("string");
+
+            log.info("Echo Int: {} String: {}", anInt.orElse(-1), string.orElse("haus"));
+          });
 
   @BeforeEach
   void setup() {
-    echoNode.setExecutor(context -> log.info("This echo node has an executor but not a value!"));
-
-    stringLogNode.setExecutor(context -> log.info("Echo {}", context.getBag().get("string").orElseThrow()));
-    integerLogNode.setExecutor(context -> log.info("Int Echo: {}", context.getBag().get("int").orElseThrow()));
-
     echoNode.addNode(integerLogNode);
     echoNode.addNode(stringLogNode);
 
     testNode.addNode(echoNode);
+    testNode.addNode(createNode);
+
+    createNode.addNode(optionalNode);
+    createNode.addNode(integerLogNode);
+    integerLogNode.addNode(stringLogNode);
+    stringLogNode.addNode(optionalNode);
 
     solver.registerCommand(testNode);
+  }
+
+  @Test
+  void shouldFindOptionalNodeWithValue() {
+    CommandRoute actual = solver.findRoute("test create 10 test optional");
+
+    assertEqualsRoute(actual, testNode, createNode, integerLogNode, stringLogNode, optionalNode);
+  }
+
+  @Test
+  void shouldFindOptionalNodeWithoutValues() {
+    CommandRoute actual = solver.findRoute("test create optional");
+
+    assertEqualsRoute(actual, testNode, createNode, optionalNode);
   }
 
   @Test
