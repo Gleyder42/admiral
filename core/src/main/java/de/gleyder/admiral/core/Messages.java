16fb875606df53be58d7077b525db2fe0b4aab3b
Gleyder <Gleyder@t-online.de>
2020-08-19 16:14:57 +0200

    Changed error messages
    Changed `command not found` messages to be more generic.
    A command can exists, but slightly different. E.g a command
    could need an int, but a string is provided.

--- core/src/main/java/de/gleyder/admiral/core/Messages.java
+++ core/src/main/java/de/gleyder/admiral/core/Messages.java
@@ -7,11 +7,12 @@ import lombok.Setter;
 @AllArgsConstructor
 public enum Messages {
 
-  NO_COMMAND_FOUND("No command found", "command.no-command"),
+  NO_COMMAND_FOUND("No matching command could be found for the specified parameters", "command.no-command"),
   NO_EXECUTOR_ON_ROUTE("The route has no executor", "command.no-executor"),
   FURTHER_ARGUMENTS_REMAIN("Further arguments remain [%s] but command tree has finished with node %s",
       "command.arguments-remain"),
-  MULTIPLE_COMMANDS_FOUND("Multiple commands found", "command.multiple-commands");
+  MULTIPLE_COMMANDS_FOUND("Several commands were found that match the specified parameters",
+      "command.multiple-commands");
 
   @Setter
   private String message;
